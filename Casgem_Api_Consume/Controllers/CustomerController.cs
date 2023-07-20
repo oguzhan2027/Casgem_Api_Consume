12aa6bc7cc5f3ce06dbf7d05e46352ac6abebe74
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-07-20 10:26:32 +0300

    customer

---
+++ Casgem_Api_Consume/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using Casgem_Api_Consume.Dtos.CustomerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Casgem_Api_Consume.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CustomerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44396/api/Customer/");
+                if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCustomerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCustomer(CreateCustomerDto createCustomerDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCustomerDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44396/api/Customer",stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
